2b574c48d4ae4379a037b8f6786d7a22b59951a3
techano <kevin.topete.kt@gmail.com>
2019-10-19 15:09:59 -0700

    Update Melon-simulator.lua

--- Melon-simulator.lua
+++ Melon-simulator.lua
@@ -1,6 +1,7 @@
 -- for https://www.roblox.com/games/4135217113/Melon-Simulator
 -- constantly tps melons to you that spawn even if areas
 -- arent unlocked (just afk while script is running)
+-- by using you agree to the GNU General Public License v3.0 
 local p = game.Players.LocalPlayer
 local char = p.Character
 
