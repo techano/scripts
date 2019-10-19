e8d19ad2367f93a73e6d2038c9fedcb45abb02d8
techano <kevin.topete.kt@gmail.com>
2019-10-19 13:23:04 -0700

    Create Melon-simulator.lua

---
+++ Melon-simulator.lua
@@ -0,0 +1,14 @@
+-- for https://www.roblox.com/games/4135217113/Melon-Simulator
+-- constantly tps you to the melons that spawn even if areas
+-- arent unlocked (just afk while script is running)
+local p = game.Players.LocalPlayer
+local char = p.Character
+
+while true do 
+	local melons = workspace.Melons:GetChildren()
+	for i, child in ipairs(melons) do
+		char.HumanoidRootPart.CFrame = child.CFrame
+		wait()
+	end
+	wait()
+end
