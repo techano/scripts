96e47d32c2e6018c48779880d58ecc8a6bd0bd6c
techano <kevin.topete.kt@gmail.com>
2020-05-02 15:39:54 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Execute this script with sudo!
+#
+read -p '/var/www/html/networkmanager' directory
+read -p 'https://github.com/techano/scripts/raw/master/' downloadlink
+#
+#
+cd /tmp
+#
+#
+apt-get install -y software-properties-common
+#
+add-apt-repository ppa:ondrej/php
+#
+apt-get update
+apt-get install -y sudo apache2 nano zip
+#
+apt-get install -y php7.3 php7.3-cli php7.3-cgi php7.3-curl php7.3-mysql php7.3-common php7.3-pdo php7.3-xml php7.3-mbstring libapache2-mod-php7.3
+#
+a2enmod rewrite
+service apache2 restart
+#
+wget -O Webbie.zip $downloadlink
+mkdir $directory
+unzip /tmp/Webbie.zip -d $directory
+#
+chmod 777 $directory/protected/config.ini
+chmod 777 $directory/protected/settings.json
+chmod 777 -R $directory/inc/php/dep/languages
+#
+exit
